b2775f3153ca434d198723cb3dd13653b94dc322
Felipe Araujo <felipearaujodeveloper@gmail.com>
2024-10-29 02:29:30 -0300

    add bufferline plugin and keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,7 +1,13 @@
 local map = vim.keymap.set
 
-map('n', '<leader>bd', '<cmd>bd<CR>', { desc = '[B]uffer [D]elete' })
-map('n', '<Esc>', '<cmd>nohlsearch')
+map('n', '<Esc>', ':nohlsearch<CR>')
+
+-- Buffers
+map('n', '<leader>bd', ':bd<CR>', { desc = '[B]uffer [D]elete' })
+map('n', '<S-h>', ':bprevious<CR>')
+map('n', '<S-l>', ':bnext<CR>')
+
+-- Windows
 map('n', '<C-h>', '<C-w><C-h>')
 map('n', '<C-l>', '<C-w><C-l>')
 map('n', '<C-j>', '<C-w><C-j>')
